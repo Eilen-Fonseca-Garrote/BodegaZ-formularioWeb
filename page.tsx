a32aee441eb5a9457d6f9bf265ef7d20fc10e14d
Eilen Fonseca Garrote <laneko27@gmail.com>
2026-04-22 15:35:26 -0400

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/next'
+import './globals.css'
+
+const _geist = Geist({ subsets: ["latin"] });
+const _geistMono = Geist_Mono({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: 'Ofertas de empleo Bodega Z Formulario',
+  description: 'Formulario de solicitud de empleo para trabajar en Bodega Z - Las Mejores Ofertas',
+  generator: 'v0.app',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className="font-sans antialiased">
+        {children}
+        {process.env.NODE_ENV === 'production' && <Analytics />}
+      </body>
+    </html>
+  )
+}
